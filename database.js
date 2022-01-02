7888b5471ff79154b5007560ec846a0fba96a8ad
hugosmoreira <hugo.n4@gmail.com>
2022-01-02 01:36:13 -0800

    .

---
+++ database.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+
+
+class Database {
+    
+    constructor() {
+        this.connect();
+    }
+
+
+    connect() {
+        mongoose.connect("mongodb+srv://admin:admin123456@stuck-app.mysz6.mongodb.net/stuck-app?retryWrites=true&w=majority")
+        .then(() => {
+            console.log("database connection successful")
+        })
+        .catch((err) => {
+            console.log("database connection error" + err)
+        })
+    }
+}
+
+
+module.exports = new Database()
