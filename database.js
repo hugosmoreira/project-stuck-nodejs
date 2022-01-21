f78e71aa65954c0f9d8d2ca4257a8d9c1b8df0b1
hugosmoreira <hugo.n4@gmail.com>
2022-01-20 16:41:11 -0800

    update bug fix

--- database.js
+++ database.js
@@ -10,7 +10,7 @@ class Database {
 
 
     connect() {
-        mongoose.connect("")
+        mongoose.connect("mongodb+srv://admin:admin123456@stuck-app.mysz6.mongodb.net/stuck-app?retryWrites=true&w=majority")
         .then(() => {
             console.log("database connection successful")
         })
