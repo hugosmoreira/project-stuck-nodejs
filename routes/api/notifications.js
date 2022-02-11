18436542bf0d62b87a834257526a17955f1c7835
hugosmoreira <hugo.n4@gmail.com>
2022-02-10 16:25:26 -0800

    update notifications

--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -11,8 +11,14 @@ const Notification = require('../../schemas/NotificationSchema');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", async (req, res, next) => {
+
+    var searchObj = { userTo: req.session.user._id, notificationType: { $ne: "newMessage" } };
+
+    if(req.query.unreadOnly !== undefined && req.query.unreadOnly == "true") {
+        searchObj.opened = false;
+    }
     
-    Notification.find({ userTo: req.session.user._id, notificationType: { $ne: "newMessage" } })
+    Notification.find(searchObj)
     .populate("userTo")
     .populate("userFrom")
     .sort({ createdAt: -1 })
@@ -27,6 +33,17 @@ router.get("/", async (req, res, next) => {
 router.put("/:id/markAsOpened", async (req, res, next) => {
     
     Notification.findByIdAndUpdate(req.params.id, { opened: true })
+    .then(() => res.sendStatus(204))
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+
+})
+
+router.put("/markAsOpened", async (req, res, next) => {
+    
+    Notification.updateMany({ userTo: req.session.user._id }, { opened: true })
     .then(() => res.sendStatus(204))
     .catch(error => {
         console.log(error);
